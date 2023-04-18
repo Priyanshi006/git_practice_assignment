7d4ebb52cdb5ff9325195a43e53bb10df09bffa5
balakrishan <abc@gmail.com>
2023-04-19 02:06:44 +0530

    assignment_problem

---
+++ palindrome.js
@@ -0,0 +1,9 @@
+let arrvalue= str.split('');
+    let reversearrvalue= arrvalue.reverse();
+    let reversestr= reversearrvalue.join('');
+    
+    
+    }
+    else{
+        console.log("No");
+    }
---
+++ prime_number.js
@@ -0,0 +1,15 @@
+let factor=0;
+    
+    for(let i =1; i<=num; i++){
+        
+            factor++;
+        }
+    }
+    //console.log(factor);
+    
+    if(factor===2){
+        console.log("Yes");
+    }
+    else{
+        console.log("No");
+    }
